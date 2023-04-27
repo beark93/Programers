73577d069ee6e956869c1cc409e0ecf1534ee379
beark93 <45477586+beark93@users.noreply.github.com>
2023-04-27 20:33:05 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,17 +1,15 @@
 function solution(n)
 {
-    /*
-    // 내 풀이
-    const arr = []
+//     // 내 풀이
+//     const arr = []
 
-    while (n > 0) {
-        const x = n % 10
-        arr.push(x)
-        n = (n - x) / 10
-    }
+//     while (n > 0) {
+//         const x = n % 10
+//         arr.push(x)
+//         n = (n - x) / 10
+//     }
 
-    return arr.reduce((a, b) => a + b)
-    */
+//     return arr.reduce((a, b) => a + b)
     
     // 다른 풀이 참고
     return n.toString().split('').map(s => parseInt(s)).reduce((a, b) => a + b)
