93d5c87247c1d20e2f3576b0b683f75608d881c6
beark93 <45477586+beark93@users.noreply.github.com>
2023-05-21 19:34:49 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    const x = Math.floor(Math.sqrt(n))
-    
     let answer = 0
+    
+    // 약수는 짝을 이루기 때문에 제곱근 까지 돌면서 더해준다.
+    const x = Math.floor(Math.sqrt(n))
     for (let i=0; i <= x; i++) {
         if ((n % i) === 0) {
+            // 제곱근이라면 i만 더하고 제곱근이 아니라면 짝을 모두 더해준다.
             if (i === n/i) answer += i
             else answer += i + n/i
         }
