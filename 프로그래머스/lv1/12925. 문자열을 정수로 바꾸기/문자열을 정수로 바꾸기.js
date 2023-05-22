af1adb38d31dcb01497b8379b2922797719ea46e
beark93 <45477586+beark93@users.noreply.github.com>
2023-05-22 21:46:52 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,4 +1,6 @@
 function solution(s) {
+    // 내풀이
+    // 문자열 하나하나 확인해서 정수로 변환
     const answer = []
     let sign = 1
     s.split('').forEach((c) => {
@@ -8,5 +10,7 @@ function solution(s) {
     })
     return answer.join('') * sign
     
+    // // 다른사람 풀이 참고
+    // // JS에서 제공하는 함수 사용
     // return parseInt(s)
 }
